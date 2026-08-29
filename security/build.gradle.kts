80ac6420d3e35d5cdcc2c1b668de5c36f2acedd1
Quant-TheodoreFelix <53819958+Quant-TheodoreFelix@users.noreply.github.com>
2026-08-29 13:02:19 +0900

    JMH 벤치마킹 및 annotation-processor 모듈 제거 - 빌드 파일 수정

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -7,24 +7,24 @@ dependencies {
     implementation(project(":annotations"))
 
     // JMH
-    jmh("org.openjdk.jmh:jmh-core:1.37")
-    jmh("org.openjdk.jmh:jmh-generator-annprocess:1.37")
-    // Source: https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-core
-    testImplementation("org.openjdk.jmh:jmh-core:1.37")
-    // Source: https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-generator-annprocess
-    testImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+//    jmh("org.openjdk.jmh:jmh-core:1.37")
+//    jmh("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+//    // Source: https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-core
+//    testImplementation("org.openjdk.jmh:jmh-core:1.37")
+//    // Source: https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-generator-annprocess
+//    testImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 }
 
-sourceSets {
-    named("jmh") {
-        java {
-            srcDirs("src/benchmark/java")
-        }
-        resources {
-            srcDirs("src/benchmark/resources")
-        }
-    }
-}
+//sourceSets {
+//    named("jmh") {
+//        java {
+//            srcDirs("src/benchmark/java")
+//        }
+//        resources {
+//            srcDirs("src/benchmark/resources")
+//        }
+//    }
+//}
 
 tasks.jar {
     from("../entlib-native/dist") {
@@ -43,30 +43,30 @@ tasks.jar {
 //
 // JMH - start
 //
-jmh {
-    jmhVersion.set("1.37")
-
-    // 테스트 벤치마킹 클래스 등록
-    includeTests.set(true)
-    includes.set(listOf(".*_JMHBenchmark"))
-
-    // 벤치마크 실행 시 필요한 jvm 인자 중앙 제어
-    jvmArgs.set(listOf(
-        "--enable-native-access=ALL-UNNAMED",
-        "--enable-preview",
-        "-Djava.library.path=${rootDir}/native-benchmark/target/debug",
-        "-Xms2g", "-Xmx2g" // gc 간섭 최소화
-    ))
-    fork.set(1)
-
-    // 결과 출력 포맷
-    resultFormat.set("JSON")
-
-    // 벤치마크 수행 옵션 (프로세스 및 반복 횟수)
-    fork.set(1)
-    warmupIterations.set(3)
-    iterations.set(5)
-}
+//jmh {
+//    jmhVersion.set("1.37")
+//
+//    // 테스트 벤치마킹 클래스 등록
+//    includeTests.set(true)
+//    includes.set(listOf(".*_JMHBenchmark"))
+//
+//    // 벤치마크 실행 시 필요한 jvm 인자 중앙 제어
+//    jvmArgs.set(listOf(
+//        "--enable-native-access=ALL-UNNAMED",
+//        "--enable-preview",
+//        "-Djava.library.path=${rootDir}/native-benchmark/target/debug",
+//        "-Xms2g", "-Xmx2g" // gc 간섭 최소화
+//    ))
+//    fork.set(1)
+//
+//    // 결과 출력 포맷
+//    resultFormat.set("JSON")
+//
+//    // 벤치마크 수행 옵션 (프로세스 및 반복 횟수)
+//    fork.set(1)
+//    warmupIterations.set(3)
+//    iterations.set(5)
+//}
 //
 // JMH - end
 //
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,4 @@ rootProject.name = "entanglementlib"
 include("core")
 include("security")
 include("annotations")
-include("annotation-processor")
 include("internal-shared-server")
